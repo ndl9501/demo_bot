bed0820b532d7e8bddb32fbd434f0793d37c1f92
ndl9501 <nduylong9501@gmail.com>
2021-06-15 23:40:42 +0700

    add js...

---
+++ commands/server/ss.js
@@ -0,0 +1,22 @@
+// const simsimi = require('simsimi')({
+//     key: 'bqRtI0Xws4h7~1~je~4OuBi-.~VCBfb.JfqAwXj8',
+//     api: 'http://sandbox.api.simsimi.com/request.p',
+//     lc: 'vn',
+//     ft: '1.0'
+// });
+
+// module.exports = {
+//     name: 'ss',
+//     category : 'server',
+//     aliases : ['ss'],
+//     run : (client, message , args)=>{
+//         const msg = args.join(' ');
+        
+          
+//         (async () => {
+//             const response = await simsimi(msg);
+//             message.channel.send('-' +  response);
+//         })();  
+          
+//     }
+// }
