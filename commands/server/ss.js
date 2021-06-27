4b3ca9901ecbdb578b6d05b587b8ebf4c0a668a2
ndl9501 <nduylong9501@gmail.com>
2021-06-27 12:05:37 +0700

    cm
    fix er music, chat bot ss

--- commands/server/ss.js
+++ commands/server/ss.js
@@ -1,22 +1,26 @@
-// const simsimi = require('simsimi')({
-//     key: 'bqRtI0Xws4h7~1~je~4OuBi-.~VCBfb.JfqAwXj8',
-//     api: 'http://sandbox.api.simsimi.com/request.p',
-//     lc: 'vn',
-//     ft: '1.0'
-// });
+const axios = require('axios');
+const { MessageEmbed, Client } = require("discord.js")
 
-// module.exports = {
-//     name: 'ss',
-//     category : 'server',
-//     aliases : ['ss'],
-//     run : (client, message , args)=>{
-//         const msg = args.join(' ');
-        
-          
-//         (async () => {
-//             const response = await simsimi(msg);
-//             message.channel.send('-' +  response);
-//         })();  
-          
-//     }
-// }
+
+module.exports = {
+    name: 'ss',
+    category : 'server',
+    aliases : ['ss'],
+    run : async (client, message , args)=>{
+        //key: 'f~K3mBYhQrfjP1FwUxfC0ga0Tl3BrZ~tji3Chd4d',
+        // https://api.nasa.gov/planetary/apod?api_key=1hXc1dZuiJ3aa8MXgivp8X8XkMduLC0cMf7skX6Q
+        const str = args.join(' ');
+        console.log(str);
+        const uri = "https://tuanxuong.com/api/simsimi/index.php?text="+str;
+        const url = encodeURI(uri);
+        await axios.get(url)
+        .then(rs => {
+            console.log(rs.data.response);
+            message.channel.send(rs.data.response);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
+}
+
